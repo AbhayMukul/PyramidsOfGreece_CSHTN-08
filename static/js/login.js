768dea56344465361edac6ce11e183e9de34975a
saanika <tatuskarsaanika@gmail.com>
2021-09-24 20:43:19 +0530

    sixth commit

--- static/js/login.js
+++
@@ -1,9 +0,0 @@
-function validateform(){  
-    var email=document.login.email.value;  
-    var password=document.login.password.value;  
-      
-     if(password.length<6){  
-      alert("Password must be at least 6 characters long.");  
-      return false;  
-      }  
-    }  
--- static/js/signup.js
+++ static/js/signup.js
@@ -1,16 +1,27 @@
 var form = document.getElementById('signup')
 form.addEventListener('submit',function(e){
     e.preventDefault()
-
+    var username = document.getElementById('username').value
+    var email = document.getElementById('email').value
+    var mobile = document.getElementById('mobile').value
     var password = document.getElementById('password').value
     var confirmpassword = document.getElementById('confirmpassword').value
-    if(password==confirmpassword){  
-        return true;  
-        }  
-        else{  
-        alert("password must be same!");  
-        return false;  
-        }  
-        }  
 
-)
+    fetch("ec2-3-109-123-120.ap-south-1.compute.amazonaws.com:5000/api/uploadUser",{
+        method:'POST',
+        body:JSON.stringify({
+                "username" : username,
+                "password" : password,
+                "email" : email,
+                "phone" : mobile 
+         }),
+   
+  
+    })
+   .then(function(response){
+       return response.json()
+   })
+   .then(function(data){
+       console.log(data)
+   })
+})
